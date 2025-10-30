1ae6f63c88caa0f729a65ade18ad5ddc05db8ce0
Shazzad_softzino <shazzad@softzino.com>
2025-10-30 12:21:27 +0600

    update jsx

---
+++ app/posts/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+export default function Posts() {
+  return (
+   <main className="p-6">
+      <div>
+        
+            <h1 className="text-5xl">All Posts</h1> 
+
+      </div>
+   </main>
+  );
+}
